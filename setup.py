32b645ce668bc873504e384fcb3029319ceb0796
Rocky Meza <rocky@fusionbox.com>
2012-10-11 22:24:25 -0600

    added simple README

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
 
 __doc__ = """
 WiFi tools that could possibly work on a nice day, if you are lucky.
 """
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 install_requires = []
 try:
@@ -16,9 +22,10 @@ version = '0.0.1'
 setup(
     name='wifi',
     version=version,
-    description=__doc__,
     author='Rocky Meza, Gavin Wahl',
     author_email='rockymeza@gmail.com',
+    description=__doc__,
+    long_description=read('README.md'),
     packages=['wifi'],
     scripts=['bin/wifi'],
     platforms="Debian",
