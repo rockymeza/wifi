10c5c54490423cde7545dba72e7e81caa10bb1ab
alexykot <alexykot@gmail.com>
2013-12-24 22:11:46 +0000

    - exceptions module added - exception renamed

---
+++ wifi/exceptions.py
@@ -0,0 +1,3 @@
+
+class InterfaceError(Exception):
+    pass
--- wifi/utils.py
+++ wifi/utils.py
@@ -38,6 +38,3 @@ def print_table(matrix, sep='  ', file=sys.stdout, *args, **kwargs):
     for row in matrix:
         print(format.format(*row).strip(), file=file, *args, **kwargs)
 
-
-class InterfaceFail(Exception):
-    pass
