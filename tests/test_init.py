c3e7cdb0df6f52831aa254c47a1206092c3360c9
Vladyslav Sitalo <root@stvad.org>
2016-02-11 15:00:02 +0100

    Init test

---
+++ tests/test_init.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from wifi.scan import Cell
+
+class CorrectInitTest(TestCase):
+    fields = {"ssid": None,
+              "bitrates": [],
+              "address": None,
+              "channel": None,
+              "encrypted": False,
+              "encryption_type": None,
+              "frequency": None,
+              "mode": None,
+              "quality": None,
+              "signal": None}
+
+
+    def test_empty_init(self):
+        tcell = Cell()
+
+        for field, value in self.fields.iteritems():
+            self.assertEqual(value, getattr(tcell, field))
