2b34a72b6747e90bdadae0393df6aa0216a93910
Melvyn Sopacua <melvyn@unikotec.com>
2017-07-13 18:03:04 +0200

    Bugfix: wpa parsing fails sometimes
    Since encryption_type is set in __init__ the hasattr test always
    fails. Convert to is None check.

--- wifi/scan.py
+++ wifi/scan.py
@@ -158,9 +158,9 @@ def normalize(cell_block):
                 while lines and lines[0].startswith(' ' * 4):
                     values.append(lines.pop(0).strip())
 
-                if 'WPA2' in value and not hasattr(cell, 'encryption_type'):
+                if 'WPA2' in value and cell.encryption_type is not None:
                     cell.encryption_type = 'wpa2'
-                elif 'WPA' in value and not hasattr(cell, 'encryption_type'):
+                elif 'WPA' in value and cell.encryption_type is not None:
                     cell.encryption_type = 'wpa'
                 elif 'WPA' in value and cell.encryption_type == 'wpa2' or 'WPA2' in value and cell.encryption_type == 'wpa':
                     cell.encryption_type = 'wpa/wpa2'
